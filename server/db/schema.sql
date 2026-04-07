de7815e1ce13eaf08ec0fe96a17448645c7c4c2a
Kiana Mills <kiana.mills20@gmail.com>
2026-04-06 18:59:52 -0700

    added categories, events, and event_attendees tables to schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,6 +1,43 @@
-DROP TABLE IF EXISTS users; 
+DROP TABLE IF EXISTS event_attendees;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username text UNIQUE NOT NULL
+
+CREATE TABLE  users(
+   id SERIAL PRIMARY KEY,
+   username text UNIQUE NOT NULL,
+   password text NOT NULL,
+   role text NOT NULL,
+   created_at timestamp DEFAULT NOW()
+);
+
+
+CREATE TABLE categories(
+   id SERIAL PRIMARY KEY,
+   name TEXT UNIQUE NOT NULL
+);
+
+
+CREATE TABLE events(
+   id SERIAL PRIMARY KEY,
+   title text NOT NULL,
+   description text NOT NULL,
+   creator_id INT NOT NULL,
+   category_id INT NOT NULL,
+   location text NOT NULL,
+   date timestamp NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+   created_at timestamp DEFAULT NOW()
+);
+
+
+CREATE TABLE event_attendees(
+   user_id INT NOT NULL,
+   event_id INT NOT NULL,
+   PRIMARY KEY (user_id, event_id),
+   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+   FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE,
+   joined_at timestamp DEFAULT NOW()
 );
