8d5aff948fbdcca139e335a6f5477f4d61bf26d7
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 15:18:09 -0700

    added requireBody middlewarefor /register

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,13 @@
 import express from "express";
 import { createUser } from "../db/queries/users.js";
+import requireBody from "../middleware/requireBody.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", requireBody(["username", "password"]), register);
 
 async function register(req, res) {
   try {
-    console.log("HEADERS:", req.headers);
-    console.log("BODY:", req.body);
-
-    if (!req.body) {
-      return res.status(400).json({ error: "req.body is undefined" });
-    }
     const { username, password } = req.body;
 
     const user = await createUser(username, password, "user");
