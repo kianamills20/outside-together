5dddc367677783324fa3a17ef1ebf895f414e742
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 16:58:52 -0700

    created folders and files to start frontend & started my app.jsx and main.jsx routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,16 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-  const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3001";
-
-  useEffect(() => {
-    async function getGreeting() {
-      const response = await fetch(`${API_BASE}/greet`);
-      if (response.ok) {
-        const json = await response.json();
-        console.log(json);
-        setMessage(json.username || "No username found");
-      }
-    }
-    getGreeting();
-  }, []);
-
   return (
     <>
-      <h1>{message}</h1>;
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/api/auth.js
@@ -0,0 +1,3 @@
+// fetch to backend: register & login + token
+
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3001";
