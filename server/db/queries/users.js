58e8f7110a6c5d26cd6a0c7b700c71d1a78b741a
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 13:18:55 -0700

    added query files and updated createUser query

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,7 +1,17 @@
 import db from "../client.js";
+import bcrypt from "bcrypt";
 
-export async function getUsers(){
-    const SQL = "SELECT username FROM users;";
-    const { rows: [users] } = await db.query(SQL);
-    return users;
+export async function createUser(username, password) {
+    const SQL = `
+    INSERT INTO users
+    (username, password)
+    VALUES ($1, $2)
+    RETURNING *
+    `;
+    const hashedPassword = await bcrypt.hash(password, 15);
+    const {
+        rows: [user],
+    } = await db.query(SQL, [username, hashedPassword]);
+    return user;
 }
+
--- server/db/seed.js
+++ server/db/seed.js
@@ -3,7 +3,7 @@ import db from "./client.js";
 await db.connect();
 await seed();
 await db.end();
-console.log("database seeded");
+console.log("🌱 Database seeded.");
 
 async function seed() {
   const SQL = "INSERT INTO users (username) VALUES ($1) RETURNING *";
