2fcf59946b35eadbc713db1d7e44d73332dce100
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 15:59:08 -0700

    added JWT into my login route and token passed on postman

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,7 @@ import {
   getUserByUsernameAndPassword,
 } from "../db/queries/users.js";
 import requireBody from "../middleware/requireBody.js";
+import { createToken } from "../utils/jwt.js";
 
 const router = express.Router();
 
@@ -15,7 +16,6 @@ async function register(req, res) {
     const { username, password } = req.body;
 
     const user = await createUser(username, password, "user");
-
     res.status(201).json(user);
   } catch (error) {
     console.log(error);
@@ -31,7 +31,15 @@ async function login(req, res, next) {
     if (!user) {
       return res.status(401).send("Invalid username or password");
     }
-    res.status(200).json(user);
+    const token = createToken({ id: user.id });
+    res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        role: user.role
+      }
+    });
   } catch (error) {
     next(error);
   }
