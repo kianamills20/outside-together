468617e4b5dd1fd9f2ad14e9b75ac8267003a395
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 15:44:43 -0700

    added /login route and query and passed test through postman

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -2,16 +2,32 @@ import db from "../client.js";
 import bcrypt from "bcrypt";
 
 export async function createUser(username, password, role) {
-    const SQL = `
+  const SQL = `
     INSERT INTO users
     (username, password, role)
     VALUES ($1, $2, $3)
     RETURNING *
     `;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const {
-        rows: [user],
-    } = await db.query(SQL, [username, hashedPassword, role]);
-    return user;
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const {
+    rows: [user],
+  } = await db.query(SQL, [username, hashedPassword, role]);
+  return user;
 }
 
+export async function getUserByUsernameAndPassword(username, password) {
+  const SQL = `
+    SELECT *
+    FROM users
+    WHERE username = $1
+    `;
+  const {
+    rows: [user],
+  } = await db.query(SQL, [username]);
+  if (!user) return null;
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return null;
+
+  return user;
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createUser } from "../db/queries/users.js";
+import {
+  createUser,
+  getUserByUsernameAndPassword,
+} from "../db/queries/users.js";
 import requireBody from "../middleware/requireBody.js";
 
 const router = express.Router();
 
 router.post("/register", requireBody(["username", "password"]), register);
+router.post("/login", requireBody(["username", "password"]), login);
 
 async function register(req, res) {
   try {
@@ -12,10 +16,24 @@ async function register(req, res) {
 
     const user = await createUser(username, password, "user");
 
-    res.status(200).json(user);
+    res.status(201).json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
+  }
+}
+
+async function login(req, res, next) {
+  try {
+    const { username, password } = req.body;
+    const user = await getUserByUsernameAndPassword(username, password);
+
+    if (!user) {
+      return res.status(401).send("Invalid username or password");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
   }
 }
 
