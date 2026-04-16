d3434f17c6083767a8421c707f2fa9caf02ac90e
Kiana Mills <kiana.mills20@gmail.com>
2026-04-15 17:17:48 -0700

    added basic functions for home, login, and register to see rendering on local host

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      <Navbar />
+      <NavBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
