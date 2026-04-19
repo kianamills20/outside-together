6d748df70a1524b9d128edcc125587ba4920bfa7
Kiana Mills <kiana.mills20@gmail.com>
2026-04-19 01:40:45 -0700

    added Dashboard.jsx and Account.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,22 @@
 import { useAuth } from "../auth/AuthContext";
+import { Link } from "react-router-dom"
 
 export default function Dashboard() {
 
-    const { user } = useAuth();
+  const { user, token } = useAuth();
+
+  if (!token) {
+    return (
+      <>
+        <h1>Account</h1>
+        <p>You're not logged in.</p>
+        <div>
+          <Link to="/login">Log in</Link>
+          <Link to="/register">Register if you don't have an account.</Link>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
