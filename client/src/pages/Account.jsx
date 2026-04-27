0a9952baa8de3f846d47a1bfc6828734ad39578c
Kiana Mills <kiana.mills20@gmail.com>
2026-04-27 15:38:18 -0700

    added handleSubmitted and form to account page

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,8 +1,38 @@
+import { changePassword } from "../api";
 import { useAuth } from "../auth/AuthContext";
 import { Link } from "react-router-dom";
 
 export default function Account() {
   const { user, token } = useAuth();
+
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit(formData) {
+    setMessage("");
+    setError("");
+
+    const currentPassword = formData.get("currentPassword");
+    const newPassword = formData.get("newPassword");
+    const confirmPassword = formData.get("confirmPassword");
+
+    if (newPassword !== confirmPassword) {
+      setError("New passwords do not match.");
+      return;
+    }
+    try {
+      const result = await changePassword(
+        {
+          currentPassword,
+          newPassword,
+        },
+        token,
+      );
+      setMessage(result.message);
+    } catch (err) {
+      setError(err.message);
+    }
+  }
 
   if (!token) {
     return (
@@ -18,11 +48,37 @@ export default function Account() {
   }
 
   return (
-    <>
-      <h1>
-        {user?.first_name} {user?.last_name}
-      </h1>
-      <p>Account settings</p>
-    </>
+    <main className="page">
+      <section className="form">
+        <h1>
+          {user?.first_name} {user?.last_name}
+        </h1>
+        <p>{user?.username}</p>
+        <h2>Change Password</h2>
+        <form action={handleSubmit}>
+          <input
+            type="password"
+            name="currentPassword"
+            placeholder="Current Password"
+            required
+          />
+          <input
+            type="password"
+            name="newPassword"
+            placeholder="New Password"
+            required
+          />
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm New Password"
+            required
+          />
+          <button>Save Password</button>
+        </form>
+        {message && <p>{message}</p>}
+        {error && <p role="alert">{error}</p>}
+      </section>
+    </main>
   );
 }
