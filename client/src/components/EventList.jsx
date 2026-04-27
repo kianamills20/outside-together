28af437fc5e15f4b9f0ce9bbc4020159a16f1204
Kiana Mills <kiana.mills20@gmail.com>
2026-04-26 22:50:44 -0700

    got all seeded events to fully render on Dashboard and PublicPage

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -1,15 +1,27 @@
-export default function EventList({ events }) {
-    return (
-        <div>
-            {events.map((event) => {
-            return <EventListItem key={event.id} event={event} />
-            })}
-        </div>
-    )
-};
+import { Link } from "react-router-dom";
 
-function EventListItem({ event }){
-    return (<>
-    
-    </>)
-};
+export default function EventList({ events }) {
+  return (
+    <div>
+      {events.map((event) => {
+        return <EventListItem key={event.id} event={event} />;
+      })}
+    </div>
+  );
+}
+
+function EventListItem({ event }) {
+  return (
+    <>
+      <Link to={"/events/" + event.id}>
+        <img src={event.image_url} alt={event.title} />
+        <div>
+          <h3>{event.title}</h3>
+          <p>{event.description}</p>
+          <p>{event.location}</p>
+          <p>{event.event_date}</p>
+        </div>
+      </Link>
+    </>
+  );
+}
--- client/src/pages/PublicPage.jsx
+++ client/src/pages/PublicPage.jsx
@@ -1,17 +1,24 @@
-import { getCategories } from "../api";
+import { getCategories, getEvents } from "../api";
 import CategoryFilter from "../components/CategoryList";
 import { useEffect, useState } from "react";
+import EventList from "../components/EventList";
 
 export default function PublicPage() {
   const [categories, setCategories] = useState([]);
+  const [events, setEvents] = useState([]);
 
-   async function loadCategories() {
-      const data = await getCategories();
-      setCategories(data);
-    }
+  async function loadCategories() {
+    const data = await getCategories();
+    setCategories(data);
+  }
+  async function loadEvents() {
+    const data = await getEvents();
+    setEvents(data);
+  }
 
   useEffect(() => {
     loadCategories();
+    loadEvents();
   }, []);
 
   return (
@@ -19,6 +26,7 @@ export default function PublicPage() {
       <h1>Landing Page</h1>
       <div>
         <CategoryFilter categories={categories} />
+        <EventList events={events} />
       </div>
     </>
   );
